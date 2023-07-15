5e1df1eb5cd5ec12921ca9c8de9dac6ed25febb7
uelkariuki <cratzuel@gmail.com>
2023-07-15 11:48:18 +0000

    Task 7 partly

--- console.py
+++ console.py
@@ -6,6 +6,8 @@ command interpreter
 """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 """ importing cmd module """
 
 
@@ -13,13 +15,66 @@ class HBNBCommand(cmd.Cmd):
     """ class for the command interpreter """
     prompt = "(hbnb) "
 
+    def do_create(self, arg):
+        """
+        Command that Creates a new instance of BaseModel, saves
+        it (to the JSON file) and prints the id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        class_name = arg.split()[0]
+
+        try:
+            class_obj = eval(class_name)
+            instance = class_obj()
+            instance.save()
+            print(instance.id)
+
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the
+        class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        cls_name = args[0]
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        id_instance = args[1]
+
+        try:
+            class_object = eval(cls_name)
+            retrieved_instances = storage.all()
+
+            key = "{}.{}".format(cls_name, id_instance)
+            Instance = retrieved_instances.get(key)
+
+            if not Instance:
+                print("** no instance found **")
+                return
+
+            print(Instance)
+
+        except NameError:
+            print("** class doesn't exist **")
+
     def emptyline(self):
         """An empty line + ENTER shouldn’t execute anything """
         pass
 
     def do_quit(self, line):
         """Quit command so as to exit the program"""
-        return exit
+        return True
 
     def do_EOF(self, line):
         """EOF command to exit the program """
