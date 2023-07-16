c5a63aafbfef19ec5c03a096d7f420abe45cbcfd
uelkariuki <cratzuel@gmail.com>
2023-07-16 11:46:04 +0000

    Task 10. Console 1.0

--- console.py
+++ console.py
@@ -8,6 +8,11 @@ command interpreter
 import cmd
 import re
 from models.base_model import BaseModel
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models.user import User
 from models import storage
 """ importing cmd module """
