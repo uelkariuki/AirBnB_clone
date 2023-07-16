f6144e84267fc4832ec94027833d39f2de843e15
uelkariuki <cratzuel@gmail.com>
2023-07-16 09:55:18 +0000

    All unittests for Task 9. More classes!

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -5,7 +5,7 @@ import models
 import uuid
 from datetime import datetime
 from unittest.mock import patch
-from models.state import State
+from models.review import Review
 from models import storage
 from models.base_model import BaseModel
 from io import StringIO
@@ -15,31 +15,37 @@ Importing the unittest module to be used in validating tests
 """
 
 """
-Unit testing for class State that defines all common
+Unit testing for class Review that defines all common
 attributes/methods for other classes
 """
 
 
-class TestState(unittest.TestCase):
+class TestReview(unittest.TestCase):
     """
-    class to validate tests to be done on the class State
+    class to validate tests to be done on the class Review
     """
 
     def test_documentation_module(self):
         """method to check the module documentation"""
-        module_doc = State.__doc__
+        module_doc = Review.__doc__
         self.assertTrue(len(module_doc) > 1, "No Module documentation")
 
     def test_documentation_class(self):
         """method to check the class(BaseModel) documentation"""
-        class_doc = State.__doc__
+        class_doc = Review.__doc__
         self.assertTrue(len(class_doc) > 1, "No class documentation")
 
     def test_create_instance(self):
         """
         Testing if the instance was created well
         """
-        my_state = State()
-        self.assertIsInstance(my_state, State)
-        self.assertTrue(hasattr(my_state, "name"))
-        self.assertIsInstance(my_state.name, str)
+        my_review = Review()
+        self.assertIsInstance(my_review, Review)
+        self.assertTrue(hasattr(my_review, "place_id"))
+        self.assertIsInstance(my_review.place_id, str)
+
+        self.assertTrue(hasattr(my_review, "user_id"))
+        self.assertIsInstance(my_review.user_id, str)
+
+        self.assertTrue(hasattr(my_review, "text"))
+        self.assertIsInstance(my_review.text, str)
